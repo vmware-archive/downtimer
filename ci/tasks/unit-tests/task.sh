1f3a57235d883923b60f07f629b3697b706d927a
Slawek Ligus <sligus@pivotal.io>
2017-02-13 17:26:29 -0800

    Add unit test tasks
    Signed-off-by: Raymond Lee <rlee@pivotal.io>

---
+++ ci/tasks/unit-tests/task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/tools/godep
+
+export GOPATH=$PWD/go:$GOPATH
+
+cd go/src/github.com/pivotal-cf/downtimer
+
+godep restore
+ginkgo -r -race
