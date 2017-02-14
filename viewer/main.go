51764b317da708f003aa5d0c950bb3ec446cae27
Nitin Ravindran <nravindran@pivotal.io>
2017-02-14 18:42:33 -0500

    Added a selection box to generate the graphs for the provided dataset under public/*.csv
    Signed-off-by: Nitin Ravindran <nravindran@pivotal.io>
    Signed-off-by: Raymond Lee <rlee@pivotal.io>

--- viewer/main.go
+++ viewer/main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("public/index.html") // Parse template file.
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	files, _ := filepath.Glob("public/*.csv")
+
+	t.Execute(w, files)
+
+}
 
 func main() {
 
 	fs := http.FileServer(http.Dir("public"))
 
-	http.Handle("/", fs)
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+	http.HandleFunc("/index.html", handler)
 
 	fmt.Println("Starting Server on 3000")
 
